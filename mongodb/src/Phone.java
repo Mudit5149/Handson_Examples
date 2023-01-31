ddc16429e0ce2ddf75ded3c65b8a15924ea1fc68
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-01-31 16:42:35 +0530

    Add files via upload

---
+++ mongodb/src/Phone.java
@@ -0,0 +1,28 @@
+package java_class_examples;
+	class Phone{
+	    public void ring(){
+	        System.out.println("Ringing...");
+	    }
+	    public void vibrate(){
+	        System.out.println("Vibrating...");
+	    }
+	    public void callFriend(){
+	        System.out.println("Calling Mukul...");
+	    }
+
+
+
+
+	public static void main(String[] args) {
+		  Phone asus = new Phone();
+	        asus.callFriend();
+	        asus.vibrate();
+	        asus.ring();
+	       
+
+	}
+		
+
+	}
+
+
